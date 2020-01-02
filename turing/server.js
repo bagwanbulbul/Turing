925e6878715a66d18b6783f43ba732543ad60a97
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-02 16:35:38 +0530

    create a new server for category

--- turing/server.js
+++ turing/server.js
@@ -3,7 +3,10 @@ const app =express();
 const department = require('./routes/department');
 app.use("/department",department);
 
-app.listen(3000,()=>{
-    console.log("server is listening 3000 ....")
+const category=require("./routes/category");
+app.use("/category",category)
+
+app.listen(4000,()=>{
+    console.log("server is listening 4000 ....")
 
 });
