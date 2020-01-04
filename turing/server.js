1df4c06d2019ebec413a6e9c2368e5703aa2a7eb
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-04 10:29:44 +0530

    created route for product

--- turing/server.js
+++ turing/server.js
@@ -9,6 +9,10 @@ app.use("/category",category)
 const attribute=require("./routes/attribute");
 app.use("/attribute",attribute)
 
+const product = require("./routes/product");
+app.use("/product",product)
+
+
 app.listen(3000,()=>{
     console.log("server is listening 3000 ....")
 
