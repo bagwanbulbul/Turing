f54649e9382ee86faccb33ae44a7d256621c925e
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-04 14:53:55 +0530

    select query

--- turing/model/productKnex.js
+++ turing/model/productKnex.js
@@ -4,4 +4,14 @@ let productData = ()=>{
     return knex.select("*").from("product")
 }
 
-module.exports={productData}
+// let add_query =knex('franchisee').where('franchisee_name', 'like','%jackmarker%').andWhere('company_name', 'like','%marker%')
+// .then((resp) => {
+//     console.log("resp >>",resp)
+// })`
+
+
+let productByID = (product_id)=>{
+    return knex.select("*").from("product").where("product_id",product_id)
+}
+
+module.exports={productData,productByID}
