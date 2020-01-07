ba85bf81f957fd44499d5c968d1593b2285a4ca8
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-07 19:07:00 +0530

    solved error

--- turing/server.js
+++ turing/server.js
@@ -12,17 +12,17 @@ app.use("/attribute",attribute)
 const product = require("./routes/product");
 app.use("/product",product)
 
-const tax = require("./routes/tax");
-app.use("/tax",tax)
+// const tax = require("./routes/tax");
+// app.use("/tax",tax)
 
-const shipping = require("./routes/shipping")
-app.use("/shipping",shipping)
+// const shipping = require("./routes/shipping")
+// app.use("/shipping",shipping)
 
 const customer = require("./routes/customer")
 app.use("/customer",customer)
 
 
-app.listen(3000,()=>{
-    console.log("server is listening 3000 ....")
+app.listen(4000,()=>{
+    console.log("server is listening 4000 ....")
 
 });
