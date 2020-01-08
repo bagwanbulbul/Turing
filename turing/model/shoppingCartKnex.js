080b97d97ec51c633f69fc792e0adf7709da7aa4
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-08 18:37:54 +0530

    query for total ammount

--- turing/model/shoppingCartKnex.js
+++ turing/model/shoppingCartKnex.js
@@ -15,6 +15,9 @@ let updateShoppingCartData=(item_id)=>{
 let deleteData=(cart_id)=>{
     return knex("shopping_cart").where("cart_id",cart_id).del()
 }
-module.exports={shoppingCartData,shoppingById,genrateUniqueId,updateShoppingCartData,deleteData}
+let totalAmmount=(cart_id)=>{
+    return knex.select("*").from("shopping_cart").join("product","product.product_id","shopping_cart.product_id").where("cart_id",cart_id)
+}
+module.exports={shoppingCartData,shoppingById,genrateUniqueId,updateShoppingCartData,deleteData,totalAmmount}
 
 // "shopping_cart.item_id","product.name","shopping_cart.attribute","product.product_id","product.price","shopping_cart.quantity"
