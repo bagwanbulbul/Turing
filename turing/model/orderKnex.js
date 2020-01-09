8c541283018223f17e65ff09ce9abd18763cdb65
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-09 13:32:39 +0530

    query for shor details of orders

--- turing/model/orderKnex.js
+++ turing/model/orderKnex.js
@@ -7,4 +7,12 @@ let orderData=(cart_id)=>{
 let postOrderData=(orderDetails)=>{
     return knex("orders").insert(orderDetails)
 }
-module.exports={orderData,postOrderData}
+
+let orderDetailData=(order_id)=>{
+    return knex.select("*").from("shopping_cart").join("product","product.product_id","shopping_cart.product_id").join("orders").where("order_id",order_id)
+}
+
+let shortDetail=(order_id)=>{
+    return knex.select("orders.order_id","orders.total_amount","orders.created_on","orders.shipped_on","orders.status","product.name").from("orders").join("product","product.product_id","orders.order_id").where("order_id",order_id)
+}
+module.exports={orderData,postOrderData,orderDetailData,shortDetail}
